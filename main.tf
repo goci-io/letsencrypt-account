49b66d257a3d317c0cb614824e0862fdb9b88705
etwillbefine <tim.gatzemeier@gmx.de>
2020-04-24 19:45:17 +0200

    run terrafrom fmt to pass checks

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "tls_private_key" "private_key" {
 }
 
 resource "acme_registration" "reg" {
-  count            = var.enabled ? 1 : 0
-  account_key_pem  = local.private_key_pem
-  email_address    = var.certificate_email
+  count           = var.enabled ? 1 : 0
+  account_key_pem = local.private_key_pem
+  email_address   = var.certificate_email
 }
